4edf7fc42285df225c01b75c03fbb118441652a1
Halil_Hasmer <halilpython@gmail.com>
2023-07-07 21:22:56 +0300

    home progress generators

--- generators/size.py
+++ generators/size.py
@@ -1,6 +1,17 @@
 import sys
 
-big_range = range(1000)
+
+def my_range(n: int):
+    print("my_range starts")
+    start = 0
+    while start < n:
+        print(f"my_range while yielding {start}")
+        yield start
+        start += 1
+
+
+# big_range = range(1000)
+big_range = my_range(5)
 
 print("big_range is {} bytes".format(sys.getsizeof(big_range)))
 
@@ -8,3 +19,6 @@ big_list = [val for val in big_range]
 
 print("big_list {} bytes".format(sys.getsizeof(big_list)))
 
+print(big_range)
+
+print(big_list)
